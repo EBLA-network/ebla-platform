8b5025d814dde40b5c84604f2d9740c07282ac93
SwissAngel <SwissAngel@eblanetwork.com>
2026-05-16 16:41:55 +0200

    chore: remove bridge address labels from explorer.

--- services/explorer/src/hooks/useAddressLabel.tsx
+++ services/explorer/src/hooks/useAddressLabel.tsx
@@ -14,12 +14,6 @@ const ADDRESS_LABELS: AddressLabels = {
   '0xd6216fc19db775df9774a6e33526131da7d19a2c': 'Kucoin 6',
   '0x0d0707963952f2fba59dd06f2b425ace40b492fe': 'Gate.io',
   '0xa605dbb535847f803227cdbaec9cdfa34d4d4b77': 'MEXC',
-  '0x39b1fc930c43606af5c353e90a55db10bcaf4087': 'Bridged ETH',
-  '0xe126e0baeae904b8cfd619be1a8667a173b763a1': 'Taraxa Bridge',
-  '0x7157233800c3c1f2ac8b12cefe2cbe796c04446b': 'Ethereum Client',
-  '0xa8f5d9fb5d1deed99f7fe175063f8d91fc65b7a4': 'Beacon Light Client',
-  '0xd014293ed981c7f557c0cf55f7fba025082ed266': 'ETH Connector',
-  '0x7c5e17a43c6cb223a86c5d63288273e0c1f1283f': 'TARA Connector',
 };
 
 export const useAddressLabel = () => {
